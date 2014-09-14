59c74bf5e404100904477deab23c06239a20e64d
Chris Carter <chris.carter@unboxedconsulting.com>
2014-09-14 18:12:12 +0100

    Try re-uglifying JS in production and remove production asset debugging.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Progress360::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -31,7 +31,6 @@ Progress360::Application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
-  config.assets.debug = true
 
   config.assets.manifest = Rails.root.join("public/assets")
 
